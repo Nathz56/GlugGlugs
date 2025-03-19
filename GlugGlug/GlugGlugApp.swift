2882813ba1d085922f656dcdefb3f03902b4901d
Frengky Gunawan <frengky.gunawan@erajaya.com>
2025-03-19 08:21:46 +0700

    Initial Commit

---
+++ GlugGlug/GlugGlugApp.swift
@@ -0,0 +1,17 @@
+//
+//  GlugGlugApp.swift
+//  GlugGlug
+//
+//  Created by Frengky Gunawan on 19/03/25.
+//
+
+import SwiftUI
+
+@main
+struct GlugGlugApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
