dca991279cf2cf0d0353520fe4dbacba9e58980e
Nur Fajar Sayyidul Ayyam <nur.fajarsa@gmail.com>
2025-03-26 10:20:33 +0700

    feat(home): create alert banner component

---
+++ GlugGlug/Views/AlertBanner.swift
@@ -0,0 +1,36 @@
+//
+//  AlertBanner.swift
+//  GlugGlug
+//
+//  Created by Nur Fajar Sayyidul Ayyam on 20/03/25.
+//
+
+import SwiftUI
+
+struct AlertBanner: View {
+    var message: String
+    var iconName: String
+    var backgroundColor: Color
+    var foregroundColor: Color
+    var textColor: Color
+    
+    var body: some View {
+        HStack {
+            Image(systemName: iconName)
+                .foregroundColor(foregroundColor)
+                .imageScale(.large)
+            
+            Text(message)
+                .font(.caption)
+                .foregroundColor(textColor)
+            
+            Spacer() // Untuk mendorong elemen ke kiri dan kanan
+        }
+        .padding()
+        .background(backgroundColor)
+    }
+}
+
+#Preview {
+    AlertBanner(message: "This is an alert!", iconName: "exclamationmark.triangle.fill", backgroundColor: .red, foregroundColor: .red, textColor: .white)
+}
