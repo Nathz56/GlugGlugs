372f65637aa2ec9201f0b08af845dc67f5d79ea5
Frengky Gunawan <frengky.gunawan@erajaya.com>
2025-03-27 11:03:32 +0700

    feat(statistic): relayout and refactor code

--- GlugGlug/StatisticView.swift
+++ GlugGlug/StatisticView.swift
@@ -14,74 +14,85 @@ struct StatisticView: View {
     @State private var weeksData: [(String, Int)] = []
     
     var body: some View {
-        VStack {
-            Picker("", selection: $selectedMode) {
-                Text("Week").tag(StatisticMode.weekly)
-                Text("Month").tag(StatisticMode.monthly)
-                Text("Year").tag(StatisticMode.yearly)
+        NavigationStack{
+            VStack {
+                Picker("", selection: $selectedMode) {
+                    Text("Week").tag(StatisticMode.weekly)
+                    Text("Month").tag(StatisticMode.monthly)
+                    Text("Year").tag(StatisticMode.yearly)
+                }
+                .pickerStyle(.segmented)
+                .padding(20)
+                
+                ChartView(selectedMode: $selectedMode)
+                Spacer()
+                HStack {
+                    Spacer()
+                    StatisticInformationView(title: "Streak", value: "7 Days", imageName: "drop.fill")
+                    Spacer()
+                    StatisticInformationView(title: "Goal Achieved", value: "3", imageName: "target")
+                    Spacer()
+                }
+                Spacer()
+                Spacer()
             }
-            .pickerStyle(.segmented)
-            .padding(20)
-            
-            ChartView(selectedMode: $selectedMode)
-            
-            Text("Statistic")
-                .font(.largeTitle)
-            Button(action: fetchData) {
-                Text("Fetch Data")
+            .toolbar {
+                ToolbarItem(placement: .navigation) {
+                    VStack(alignment: .leading) {
+                        Text("Statistics")
+                            .font(.title)
+                            .bold()
+
+                        switch selectedMode {
+                        case .weekly:
+                            Text("This Week")
+                                .font(.title2)
+                                .foregroundStyle(.gray)
+                        case .monthly:
+                            Text("This Month")
+                                .font(.title2)
+                                .foregroundStyle(.gray)
+                        case .yearly:
+                            Text("This Year")
+                                .font(.title2)
+                                .foregroundStyle(.gray)
+                        }
+                    
+                    }
+                    .padding(.top, 20)
+                }
             }
-            Button(action: {
-                HealthKitManager.shared.addWaterAmount(volume: 200)
-            }) {
-                Text("Input 200 mL")
-            }
-            Button(action: getThisWeekStatistic) {
-                Text("Get this week statistic")
-            }
-            Button(action: getThisMonthStatistic) {
-                Text("Get this month statistic")
-            }
-            Button(action: getThisYearStatistic) {
-                Text("Get this year statistic")
-            }
+            .padding(.top, 20)
         }
         .tabItem {
             Image(systemName: "chart.bar.fill")
             Text("Statistic")
         }
     }
+}
+
+struct StatisticInformationView: View {
+    let title: String
+    let value: String
+    let imageName: String
     
-    private func fetchData() {
-        HealthKitManager.shared.getConsumedWaterToday{ waterVolume in
-            if let volume = waterVolume {
-                print("Total air yang dikonsumsi hari ini: \(volume) mL")
-            } else {
-                print("Gagal membaca konsumsi air hari ini.")
+    var body: some View {
+        HStack {
+            VStack {
+                Text(title)
+                Text(value)
             }
+            Image(systemName: imageName)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 35, height: 35)
+                .foregroundColor(.blue)
+            
         }
-    }
-    
-    private func getThisWeekStatistic() {
-        HealthKitManager.shared.getThisWeekStatistic { dayDrink in
-            for (date, vol) in dayDrink {
-                print("\(date): \(vol) mL")
-            }
-        }
-    }
-    
-    private func getThisMonthStatistic() {
-        HealthKitManager.shared.getThisMonthStatistic { dayDrink in
-            for (date, vol) in dayDrink {
-                print("\(date): \(vol) mL")
-            }
-        }
-    }
-    private func getThisYearStatistic() {
-        HealthKitManager.shared.getThisYearStatistic { dayDrink in
-            for (date, vol) in dayDrink {
-                print("\(date): \(vol) mL")
-            }
-        }
+        .padding()
+        .overlay(RoundedRectangle(cornerRadius: 4).stroke(lineWidth: 2).foregroundColor(.blue))
+
+        
     }
 }
 
