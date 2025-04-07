110f981aa690b4036efbb526c69f2d437706fdaa
Frengky Gunawan <frengky.gunawan@erajaya.com>
2025-04-07 10:24:55 +0700

    feat(statistic): handle get streak

--- GlugGlug/StatisticView.swift
+++ GlugGlug/StatisticView.swift
@@ -11,7 +11,7 @@ import Charts
 struct StatisticView: View {
     @StateObject var healthKitManager = HealthKitManager.shared
     @State private var selectedMode: StatisticMode = .weekly
-    @State private var weeksData: [(String, Int)] = []
+    @State private var streak: Int = 0
     
     var body: some View {
         NavigationStack{
@@ -28,7 +28,7 @@ struct StatisticView: View {
                 Spacer()
                 HStack {
                     Spacer()
-                    StatisticInformationView(title: "Streak (Day)", value: "7", imageName: "drop.fill")
+                    StatisticInformationView(title: "Streak (Day)", value: "\(streak)", imageName: "drop.fill")
                     Spacer()
                     StatisticInformationView(title: "Goal Achieved", value: "3", imageName: "target")
                     Spacer()
@@ -63,6 +63,11 @@ struct StatisticView: View {
                 }
             }
             .padding(.top, 20)
+        }
+        .onAppear {
+            HealthKitManager.shared.getStreak { streak in
+                self.streak = streak
+            }
         }
         .tabItem {
             Image(systemName: "chart.bar.fill")
