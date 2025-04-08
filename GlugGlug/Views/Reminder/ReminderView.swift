f6c498722ec4237f937137e11badace6cf597a17
Yonathan Hilkia <yonathanhilkia@10-60-48-61.0200.bsd.id.iosda.org>
2025-04-08 10:13:16 +0700

    rapihin kode + icon change

--- GlugGlug/Views/Reminder/ReminderView.swift
+++ GlugGlug/Views/Reminder/ReminderView.swift
@@ -17,21 +17,16 @@ struct Reminder: Identifiable, Codable {
 }
 
 struct ReminderView: View {
+    
+    @EnvironmentObject var reminderViewModel: ReminderViewModel
     @Environment(\.colorScheme) var colorScheme
     @State private var menuAddAlarm = false
-    @State private var listAlarms: [Reminder] = [] {
-        didSet {
-            saveAlarms()
-        }
-    }
     @State private var selectedReminder: Reminder?
-    
-    let userDefaultKey = "savedAlarms"
     
     var body: some View {
         NavigationStack {
             VStack(alignment: .leading, spacing: 0) {
-                if listAlarms.isEmpty {
+                if reminderViewModel.listAlarms.isEmpty {
                     VStack {
                         ZStack(alignment: .bottomTrailing) {
                             Image(systemName: "alarm")
@@ -64,9 +59,9 @@ struct ReminderView: View {
             .sheet(isPresented: $menuAddAlarm) {
                 AddAlarmView { newAlarm in
                     let reminder = Reminder(time: newAlarm, isEnabled: true)
-                    listAlarms.append(reminder)
-                    sortAlarms()
-                    scheduleNotification(for: reminder)
+                    reminderViewModel.listAlarms.append(reminder)
+                    reminderViewModel.sortAlarms()
+                    reminderViewModel.scheduleNotification(for: reminder)
                 }
                 .presentationDetents([.height(350)])
             }
@@ -74,12 +69,12 @@ struct ReminderView: View {
                 EditAlarmView(reminder: Binding(
                     get: { reminder }, //ambil jamnya
                     set: { updatedReminder in
-                        if let index = listAlarms.firstIndex(where: { $0.id == updatedReminder.id }) { //nyari alarm di array dari id 0 satu satu di iterate
-                            listAlarms[index] = updatedReminder //array ketemu baru direplace sama reminder baru
-                            sortAlarms()
-                            cancelNotification(for: reminder) //cancel notif alarm sblmnya
+                        if let index = reminderViewModel.listAlarms.firstIndex(where: { $0.id == updatedReminder.id }) { //nyari alarm di array dari id 0 satu satu di iterate
+                            reminderViewModel.listAlarms[index] = updatedReminder //array ketemu baru direplace sama reminder baru
+                            reminderViewModel.sortAlarms()
+                            reminderViewModel.cancelNotification(for: reminder) //cancel notif alarm sblmnya
                             if updatedReminder.isEnabled {
-                                scheduleNotification(for: updatedReminder) //schedulebuat alarm baru
+                                reminderViewModel.scheduleNotification(for: updatedReminder) //schedulebuat alarm baru
                             }
                         }
                         selectedReminder = updatedReminder
@@ -88,8 +83,8 @@ struct ReminderView: View {
                 .presentationDetents([.height(330)])
             }
             .onAppear {
-                requestNotification()
-                loadAlarms()
+                reminderViewModel.requestNotification()
+                reminderViewModel.loadAlarms()
             }
         }
         .tabItem {
@@ -98,128 +93,40 @@ struct ReminderView: View {
         }
     }
     
-    private func deleteAlarm(at offsets: IndexSet) {
-        let remindersToDelete = offsets.map { listAlarms[$0] }
-        listAlarms.remove(atOffsets: offsets)
-        for reminder in remindersToDelete {
-            cancelNotification(for: reminder)
-        }
-        sortAlarms()
-    }
-    
-    private var timeFormatter: DateFormatter {
-        let formatter = DateFormatter()
-        formatter.timeStyle = .short
-        return formatter
-    }
-    
-    private func addReminderView() -> some View {
+    func addReminderView() -> some View {
         List {
-            ForEach(listAlarms.indices, id: \.self) { index in
+            ForEach(reminderViewModel.listAlarms.indices, id: \.self) { index in
                 HStack {
-                    Text("\(listAlarms[index].time, formatter: timeFormatter)")
+                    Text("\(reminderViewModel.listAlarms[index].time, formatter: reminderViewModel.timeFormatter)")
                         .font(.system(size: 40))
                     Spacer()
                     Toggle("", isOn: Binding(
-                        get: { listAlarms[index].isEnabled },
+                        get: { reminderViewModel.listAlarms[index].isEnabled },
                         set: { newValue in
-                            listAlarms[index].isEnabled = newValue
+                            reminderViewModel.listAlarms[index].isEnabled = newValue
                             if newValue {
-                                scheduleNotification(for: listAlarms[index])
+                                reminderViewModel.scheduleNotification(for: reminderViewModel.listAlarms[index])
                             } else {
-                                cancelNotification(for: listAlarms[index])
+                                reminderViewModel.cancelNotification(for: reminderViewModel.listAlarms[index])
                             }
                         }
                     )) .tint(.blue)
                 }
                 .padding(.vertical, 8)
                 .contentShape(Rectangle())
-                .opacity(listAlarms[index].isEnabled ? 1.0 : 0.6)
+                .opacity(reminderViewModel.listAlarms[index].isEnabled ? 1.0 : 0.6)
                 .onTapGesture {
-                    selectedReminder = listAlarms[index]
+                  selectedReminder = reminderViewModel.listAlarms[index]
                 }
             }
-            .onDelete(perform: deleteAlarm)
+            .onDelete(perform: reminderViewModel.deleteAlarm)
         }
         .listStyle(.plain)
         .listRowSpacing(15)
     }
-    
-    private func saveAlarms() {
-        if let encoded = try? JSONEncoder().encode(listAlarms) {
-            UserDefaults.standard.set(encoded, forKey: userDefaultKey)
-        }
-    }
-    
-    private func loadAlarms() {
-        if let savedData = UserDefaults.standard.data(forKey: userDefaultKey),
-           let decoded = try? JSONDecoder().decode([Reminder].self, from: savedData) {
-            listAlarms = decoded
-        }
-    }
-    
-    private func sortAlarms() {
-        listAlarms.sort { $0.time < $1.time }
-    }
-    
-    private func requestNotification() {
-        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge]) { granted, error in
-            if granted {
-                print("Notification permission granted")
-            } else if let error = error {
-                print("Error requesting permission: \(error.localizedDescription)")
-            }
-        }
-
-    }
-    
-    private func cancelNotification(for reminder: Reminder) {
-        UNUserNotificationCenter.current().removePendingNotificationRequests(withIdentifiers: [reminder.id.uuidString])
-    }
-
-    private func scheduleNotification(for reminder: Reminder) {
-        
-        guard reminder.isEnabled else { return } //cek ke enable ga jamnya, kalo ga yaudah gausah di schedule
-        
-        let randomNotifBody = [
-            "Even you are busy, don't forget it's time to rehydrate so you can perform better!",
-            "Time for a water break! Stay hydrated, stay awesome!",
-            "Glug Glug alert! Your body says thanks for the H2O!",
-            "Hydration station calling! Time to sip some water!",
-            "Water time! Keep those fluids flowing!",
-            "Ding ding! Your hydration reminder is here!",
-            "Stay fresh, stay hydrated - it's water o'clock!"
-        ]
-        
-        
-        let content = UNMutableNotificationContent()
-        content.title = "It's Glug Glug time!"
-        content.body = randomNotifBody.randomElement() ?? "It's time to rehydrate!"
-        content.sound = .default
-        
-        let calendar = Calendar.current
-        let hour = calendar.component(.hour, from: reminder.time) //block ini untuk ngetake user current calendar
-        let minute = calendar.component(.minute, from: reminder.time) //berdasarkan local timezonenya
-        
-        var dateComponents = DateComponents()
-        dateComponents.hour = hour
-        dateComponents.minute = minute
-        
-        let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: true) //trigger notif ketika sudah match dengan time yg diset di datecomponent
-        
-        let request = UNNotificationRequest(identifier: reminder.id.uuidString, content: content, trigger: trigger) //creates a notification requst
-        
-        UNUserNotificationCenter.current().add(request) { error in //access app notif manager //add request buat schedule notifnya
-            if let error = error {
-                print("❌ Error scheduling notification: \(error.localizedDescription)")
-            }
-        }
-    }
 }
-
-
-
 
 #Preview {
     ReminderView()
+        .environmentObject(ReminderViewModel())
 }
